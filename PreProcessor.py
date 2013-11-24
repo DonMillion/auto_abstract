fed287871b39d40f62450e888cd044825037cef1
Don <donlumillion@gmail.com>
2013-11-24 17:37:32 +0800

    commit my code

---
+++ PreProcessor.py
@@ -0,0 +1,59 @@
+import jieba,re
+from ResultAnalyzer import Analyzer
+
+#用结巴（jieba）分词系统分词
+#去除停用词
+
+stopwordFileLoc = 'stopwords.txt'
+SC = 0 #所有句子的总数(Sentence Count)
+
+#加载停用词表
+def loadStopwords():
+	stopwordFile = open(stopwordFileLoc)
+	stopwords = {} #遍历字典时速度会快点
+	for word in stopwordFile.read().split('\n'):
+		stopwords[word] = None
+	stopwordFile.close()
+	return stopwords
+
+STOPWORDS = loadStopwords()
+
+class sentence:
+	"""每个对象储存一个句子"""
+
+	def __init__(self, content, mode = False):
+		self.source = content
+		temp = list(jieba.cut(content, cut_all=mode))
+		self.wordcount = 0
+		#去除停用词(包括标点符号)，同时计算每个词的出现次数
+		self.segements = {}
+		for word in temp:
+			if word not in STOPWORDS:
+				if word not in self.segements:
+					self.segements[word] = {}
+					self.segements[word]['TF'] = 1
+				else:
+					self.segements[word]['TF'] += 1
+				self.wordcount += 1
+
+#"""预处理文档，同时去除停用词"""
+def process(document, mode = False):
+	#设置分段标识符号，句号、问号等
+	delimiters = r'[;.!?。？！；～\s]\s*'
+	sentences = re.split(delimiters, document)
+	result = []
+	for s in sentences:
+		item = sentence(s)
+		if item.wordcount > 0:
+			result.append(item)
+
+	global SC
+	SC = len(result)
+	return result
+
+if __name__ == '__main__':
+	result = process('预处理文档，同时去除停用的得得词哎呦。正如您可以看到的一样。每次一个函数调用另外一个函数时，在下一次发生调用时，它自己的值和状态都会被挂起')
+	for s in result:
+		print (s.segements)
+	print('\n')
+	print('SC= %s' %  SC)
---
+++ SubTopic.py
@@ -0,0 +1,152 @@
+import PreProcessor,math
+from operator import itemgetter,attrgetter
+from ResultAnalyzer import Analyzer
+
+nodeRoot = []
+SimSum = 0
+
+#计算每个单词在所有句子中的出现频率
+def calculateFrequency(sentences):
+	DocFrequency = {}
+	for sentence in sentences:
+		for word in sentence.segements:
+			if word in DocFrequency:
+				DocFrequency[word] += 1
+			else:
+				DocFrequency[word] = 1
+	return DocFrequency
+
+#计算每个特征项（词语）的权重
+def calculateWeight(sentences):
+	DocFrequency = calculateFrequency(sentences)
+	#从每个句子算
+	for sentence in sentences:
+		for word in sentence.segements:
+			TF = sentence.segements[word]['TF']/sentence.wordcount
+			IDF = math.log(PreProcessor.SC/DocFrequency[word],10) + 0.01
+			sentence.segements[word]['weight'] = TF * IDF
+	return DocFrequency
+
+#构建相似矩阵
+def buildSimilarMatrix(sentences):
+	#初始化相似矩阵，所有元素置0
+	SimMat = [[-1] * PreProcessor.SC for i in range(PreProcessor.SC)]
+	SimList = []
+	for x in range(0,PreProcessor.SC):
+		for y in range(x+1,PreProcessor.SC):
+			if x != y:
+				xSege = sentences[x].segements
+				ySege = sentences[y].segements
+				intersection = set(xSege) & set(ySege)
+
+				#余弦定理计算相似度
+				if len(intersection) == 0:
+					sim = 0
+				else:
+					print('x:%s, y:%s, inter:%s' % (x,y,intersection))
+					numerator = 0
+					for word in intersection:
+						numerator += xSege[word]['weight'] * ySege[word]['weight']
+					xlength,ylength = 0,0
+					for word in xSege:
+						xlength += xSege[word]['weight'] ** 2
+					for word in ySege:
+						ylength += ySege[word]['weight'] ** 2
+					denominator = math.sqrt(xlength) * math.sqrt(ylength)
+					sim = numerator/denominator
+				SimMat[x][y] = sim
+				SimList.append({'xy':(x,y),'sim':sim})
+
+			# else:#相同句子设为-1
+			# 	SimMat[x][y] = -1
+			# 	SimList.append({'xy':(x,y),'sim':-1})
+	return SimMat,SimList
+
+#寻找并返回节点所在树的节点
+def findRoot(n):
+	if nodeRoot[n] == -1:
+		return n
+	else:
+		nodeRoot[n] = findRoot(nodeRoot[n])
+		return nodeRoot[n]
+
+#合并两个节点所在的树，若成功，返回1,否则0
+def merge(x,y):
+	xr = findRoot(x)
+	yr = findRoot(y)
+	if xr == yr :
+		return 0
+	else:
+		global nodeRoot
+		if xr < yr:
+			nodeRoot[yr] = xr
+		else:
+			nodeRoot[xr] = yr
+	return 1
+		
+
+#生成最大生成树,kruskal算法
+def buildTree(SimList):
+	T = [] #初始化树边集为空集
+	E = sorted(SimList,key=itemgetter('sim'),reverse=True)
+	global nodeRoot,SimSum
+	nodeRoot = [-1 for i in range(PreProcessor.SC)]
+	count = 0
+	for e in E:
+		x,y = e['xy']
+		if (merge(x,y)) == 1:
+			T.append(e)
+			SimSum += e['sim']
+			count += 1
+			if count == PreProcessor.SC-1:
+				break
+	return T
+
+# def devideTree(Tree,SimMat,sentences):
+# 	global SimSum
+# 	#计算平均相似度
+# 	avg = SimSum/(PreProcessor.SC * (PreProcessor.SC - 1) / 2)
+# 	#计算顶点(句子)重要度：与其相似度大于avg的顶点的数目和顶点的度
+# 	for node in range(PreProcessor.SC):
+# 		important,d = 0,0
+
+# 		#遍历矩阵右半行
+# 		for i in range(node+1,PreProcessor.SC):
+# 			if SimMat[node][i] > 0:
+# 				d += 1
+# 				if SimMat[node][i] > avg:
+# 					important += 1
+
+# 		#遍历矩阵上半列
+# 		for i in range(0,node):
+# 			if SimMat[i][node] > 0:
+# 				d += 1
+# 				if SimMat[i][node] > avg:
+# 					important += 1
+
+# 		sentences[node].imp = important
+# 		sentences[node].d = d
+
+# 	print(SimSum)
+# 	print(avg)
+# 	#按重要度优先，度次要排序顶点（句子）
+# 	sortedSentences = sorted(sentences,key=attrgetter('imp','d'),reverse = True)
+
+# 	#选择凝聚点
+# 	Kmeans = []
+# 	for s in sortedSentences:
+		
+		
+
+def main():
+	result = PreProcessor.process('预处理文档，同时去除停用的得得词哎呦。预处理文档。每次一个函数调用另外一个函数时，在下一次发生调用时，它自己的值和状态都会被挂起')
+	doc = calculateWeight(result)
+	SimMat,SimList = buildSimilarMatrix(result)
+	print('\n')
+	for i in SimMat:
+		print(i)
+		print('\n')
+	print(buildTree(SimList))
+
+if __name__ == '__main__':
+	main()
