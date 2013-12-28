2ec611125d945e1050a5a6cec5f16e6267d722e5
Don <donlumillion@gmail.com>
2013-12-28 21:28:24 +0800

    beautify the GUI again.

--- abstract.py
+++ abstract.py
@@ -1,7 +1,10 @@
+#!/bin/python3
 # -*- coding: utf-8 -*-
 # GUI主界面
+# 同时进行输入检查
 from tkinter import *
 from tkinter import ttk
+from tkinter import font
 from tkinter import filedialog
 from tkinter import messagebox
 import getAbstract
@@ -61,6 +64,8 @@ def clip(*args):
 root = Tk()
 root.title('自动文摘生成器')
 root.option_add('*tearoff', False)
+# root.columnconfigure(0, weight=1)
+# root.rowconfigure(0, weight=1)
 
 # 设置菜单
 menubar = Menu(root)
@@ -70,7 +75,10 @@ menu_file.add_command(label='Open...', command=openFile)
 menu_file.add_command(label='Exit', command=exitProgram)
 
 # frame
-mainframe = ttk.Frame(root, padding='5 5', relief='sunken')
+mainframe = ttk.Frame(root, padding='15 15 5 15')
+print(mainframe['style'])
+# mainframe.columnconfigure(1, weight=1)
+# mainframe.rowconfigure(2, weight=1)
 
 # 显示文档的text widget
 docText = Text(mainframe, width=60, height=30)
@@ -82,12 +90,24 @@ docText.configure(yscrollcommand=docScrollbar.set)
 abstractScrollbar = ttk.Scrollbar(mainframe, orient=VERTICAL, command=abstractText.yview)
 abstractText.configure(yscrollcommand=abstractScrollbar.set)
 
+# 设置字体
+topLabelFont = font.Font(family='微软雅黑', size=10)
+showLabelFont = font.Font(family='微软雅黑', size=10)
+# buttonFont = font.Font(family='微软雅黑', size=12)
+s=ttk.Style()
+s.configure('TButton', font='微软雅黑 11 bold')
+s.configure('TButton', foreground='#6b93b5')
+styleArray = ('TButton', 'TFrame', 'TLabel', 'TEntry')
+for style in styleArray:
+	s.configure(style, background='#ebf0f6')
+ 
+
 # Label
-docLabel = ttk.Label(mainframe, text='文档内容：')
-abstractLabel = ttk.Label(mainframe, text='自动文摘：')
-percentLabel = ttk.Label(mainframe, text='压   缩   率：')
-docSCLabel = ttk.Label(mainframe, text='原文句子数：')
-abstractSCLabel = ttk.Label(mainframe, text='文摘句子数：')
+docLabel = ttk.Label(mainframe, text='文档内容：', font=topLabelFont)
+abstractLabel = ttk.Label(mainframe, text='自动文摘：', font=topLabelFont)
+percentLabel = ttk.Label(mainframe, text='压   缩   率：', font=showLabelFont)
+docSCLabel = ttk.Label(mainframe, text='原文句子数：', font=showLabelFont)
+abstractSCLabel = ttk.Label(mainframe, text='文摘句子数：', font=showLabelFont)
 docSC = StringVar()
 docSCShow = ttk.Label(mainframe, textvariable=docSC)
 abstractSC = StringVar()
@@ -107,11 +127,11 @@ root.config(menu=menubar)
 mainframe.grid(column=0, row=0, sticky=(N, S, E, W))
 docLabel.grid(column=1, row=1, sticky=(E, W))
 docText.grid(column=1, row=2, rowspan=4, sticky=(N,W), pady=5)
-docScrollbar.grid(column=2, row=2, rowspan=4, sticky=(N,S), pady=5)
+docScrollbar.grid(column=2, row=2, rowspan=4, sticky=(N,S), pady=5, padx=8)
 # 右上
 abstractLabel.grid(column=3, row=1, columnspan=4, sticky=(W, S))
 abstractText.grid(column=3, row=2, columnspan=4, sticky=(N,W), pady=5)
-abstractScrollbar.grid(column=7, row=2, sticky=(N,S), pady=5)
+abstractScrollbar.grid(column=7, row=2, sticky=(N,S), pady=5, padx=8)
 # 右下
 percentLabel.grid(column=3, row=3)
 percentEntry.grid(column=4, row=3, sticky=W)
@@ -119,8 +139,8 @@ docSCLabel.grid(column=3, row=4)
 docSCShow.grid(column=4, row=4,sticky=W)
 abstractSCLabel.grid(column=3, row=5)
 abstractSCShow.grid(column=4, row=5,sticky=W)
-runButton.grid(column=6, row=3, columnspan=2, rowspan=2)
-clipButton.grid(column=6, row=5, columnspan=2, sticky=N)
+runButton.grid(column=6, row=3, rowspan=2, sticky=E)
+clipButton.grid(column=6, row=5, sticky=(N,E))
 
 
 root.mainloop()
